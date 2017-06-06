c125511ab712ff9628097e8ee991bd1019e73199
ksmskm <ksmarvich@gmail.com>
2017-06-05 18:08:59 -0700

    refactored stop functionality of metronome

--- metronome.js
+++ metronome.js
@@ -1,28 +1,25 @@
 const audio = document.querySelector('audio[data-key="76"]');
 
-let running;
+let timer;
 let start;
 let interval;
 let time;
 
 function instance () {
-  if (running === true) {
-    time += interval;
-    var diff = (new Date().getTime() - start) - time;
-    audio.play();
-    window.setTimeout(instance, (interval - diff)); 
-  }
+  time += interval;
+  var diff = (new Date().getTime() - start) - time;
+  audio.play();
+  timer = window.setTimeout(instance, (interval - diff)); 
 }
 
 export function click (freq) {
-  running = true;
   start = new Date().getTime();
   interval = 60000 / freq; 
   time = 0;
   audio.play();
-  window.setTimeout(instance, interval);
+  timer = window.setTimeout(instance, interval);
 }
 
 export function stop () {
-  running = false;
+  window.clearTimeout(timer);
 }
